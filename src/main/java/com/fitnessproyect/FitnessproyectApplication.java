46c6ba39d908aa658b26cff8c46a118d42b4e8fa
JustinInvo <jusrtin2010@gmail.com>
2024-10-13 19:18:07 -0500

    first commit

---
+++ src/main/java/com/fitnessproyect/FitnessproyectApplication.java
@@ -0,0 +1,13 @@
+package com.fitnessproyect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessproyectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessproyectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fitnessproyect/FitnessproyectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitnessproyect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessproyectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
